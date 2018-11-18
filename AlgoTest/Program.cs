d8a40e8d9ea725451fd3d38ad4cdaff3f80e0d70
Myron William Echenim <myron.echenim@live.com>
2018-11-18 01:43:20 +0100

    simple implemtation of fisher-yate-algorithm to shuffle data

---
+++ AlgoTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Fisher_Yates_Shuffle;
+
+namespace AlgoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dat = new int[5]  { 99,  98, 92, 97, 95};
+           var result = new FisherYates().Shuffle(dat);
+            foreach (var item in result)
+            {
+                Console.Write(item+"\n");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Fisher-Yates-Shuffle/FisherYate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fisher_Yates_Shuffle
+{
+    public class FisherYates
+    {
+        private Random _rand = new Random();
+        public List<int> Shuffle(int [] data){
+            
+           var _list = new List<int>();
+
+          for(var i=0; i<data.Length; ++i){
+
+              var _rand_result = _rand.Next(i, data.Length); 
+              var _temp = data[_rand_result];
+              data[_rand_result] = data[i];
+              data[i] = _temp;
+              var resultset = data[i];
+               _list.Add(resultset);
+
+          }
+         
+         return _list;
+
+        }
+    }
+}
