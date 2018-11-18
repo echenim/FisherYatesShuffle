1d3ebacd6ae64197af59d7e662b1d27120bf5186
Myron William Echenim <myron.echenim@live.com>
2018-11-18 01:49:49 +0100

    updates fisher-yate-algorithm to shuffle data

--- AlgoTest/Program.cs
+++ AlgoTest/Program.cs
@@ -7,13 +7,17 @@ namespace AlgoTest
     {
         static void Main(string[] args)
         {
+            FisherYateShuffle();
+            Console.WriteLine("Hello World!");
+        }
+
+        static void FisherYateShuffle(){
             var dat = new int[5]  { 99,  98, 92, 97, 95};
            var result = new FisherYates().Shuffle(dat);
             foreach (var item in result)
             {
                 Console.Write(item+"\n");
             }
-            Console.WriteLine("Hello World!");
         }
     }
 }
